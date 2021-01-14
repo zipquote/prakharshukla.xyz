bf2e2fded5a91431ff83c5403674a23f6d536c0b
zipquote <prakhar.shukla321@gmail.com>
2021-01-14 13:49:14 +0530

    Setup an empty nextjs project

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Prakhar Shukla's website.</title>
+      </Head>
+      <div>Empty Slate</div>
+    </div>
+  )
+}
