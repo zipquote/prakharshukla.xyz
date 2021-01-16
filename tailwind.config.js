2d237ab0830d60fa65186cd80c557e5e58022915
zipquote <prakhar.shukla321@gmail.com>
2021-01-16 22:25:56 +0530

    Setup fonts in app

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  purge: [
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: true, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        butler: ['Butler', 'serif'],
+        'butler-bold': ['Butler Bold', 'serif'],
+        'butler-light': ['Butler Light', 'serif'],
+        'butler-medium': ['Butler Medium', 'serif'],
+        'butler-extra-bold': ['Butler Extra Bold', 'serif'],
+        poppins: ['Poppins', 'sans-serif'],
+        jetbrains: ['Jetbrains', 'sans-serif'],
+        'jetbrains-light': ['Jetbrains Light', 'sans-serif'],
+        'jetbrains-medium': ['Jetbrains Medium', 'sans-serif'],
+        'jetbrains-bold': ['Jetbrains Bold', 'sans-serif'],
+        'jetbrains-extra-bold': ['Jetbrains  Extra Bold', 'sans-serif'],
+        zoika: ['Zoika', 'sans-serif'],
+      },
+    },
   },
   variants: {
     extend: {},
