7cf9d6a76738f8f7375526f08d1c3dedb56fdaf4
zipquote <prakhar.shukla321@gmail.com>
2021-01-16 23:45:45 +0530

    Implemented Button component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,3 +1,7 @@
 export default function Button({ children }) {
-  return <button>{children}</button>;
+  return (
+    <button className="py-1 px-3 mx-1 mb-1 border-2 border-black rounded shadow-btn relative">
+      {children}
+    </button>
+  );
 }
