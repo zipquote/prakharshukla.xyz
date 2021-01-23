fd726703233153b6c0f1f1fa07b925686f47a3c5
zipquote <prakhar.shukla321@gmail.com>
2021-01-23 14:20:11 +0530

    Updated post content.

--- src/components/MDXComponents/index.js
+++ src/components/MDXComponents/index.js
@@ -1,6 +1,16 @@
 import Head from 'next/head';
 import dynamic from 'next/dynamic';
 import { NavLink } from '../';
+import catbutton from '../../../public/snippets/html/catbutton.html';
+
+function CatButton() {
+  return (
+    <div
+      className="w-80 mx-auto my-5"
+      dangerouslySetInnerHTML={{ __html: catbutton }}
+    />
+  );
+}
 
 const CodeBlock = dynamic(() => import('../CodeBlock'));
 
@@ -8,6 +18,7 @@ const MDXComponents = {
   NavLink,
   Head,
   code: CodeBlock,
+  CatButton,
 };
 
 export default MDXComponents;
