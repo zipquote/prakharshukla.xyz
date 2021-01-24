3703e444baf18817b0d801c7616cd04511f28e41
zipquote <prakhar.shukla321@gmail.com>
2021-01-24 10:31:25 +0530

    Updated card component.
    Based on type prop, Card Header is either shown or hidden.

--- src/components/BlogPostList/index.js
+++ src/components/BlogPostList/index.js
@@ -1,18 +1,22 @@
 import { Image, Card, PostDate, PostExcerpt } from '../';
 import { resolveImageDomain } from '../../utils/common';
 
-export default function BlogPostList({ frontMatter }) {
+export default function BlogPostList({ frontMatter, type }) {
   return (
     <Card className="h-full">
-      <Card.Header>
-        <Image
-          src={`${resolveImageDomain('cloudinary')}${frontMatter.cover_image}`}
-          alt={frontMatter.title}
-          type="rounded"
-          layout="fill"
-          className="card-image"
-        />
-      </Card.Header>
+      {type !== 'compact' && (
+        <Card.Header>
+          <Image
+            src={`${resolveImageDomain('cloudinary')}${
+              frontMatter.cover_image
+            }`}
+            alt={frontMatter.title}
+            type="rounded"
+            layout="fill"
+            className="card-image"
+          />
+        </Card.Header>
+      )}
       <Card.Body>
         <Card.Title>{frontMatter.title}</Card.Title>
         <PostDate postDate={frontMatter.date} />
