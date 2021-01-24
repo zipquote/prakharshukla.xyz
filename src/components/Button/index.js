42f5db24a0d812c704f4705d78e414dbda005e32
zipquote <prakhar.shukla321@gmail.com>
2021-01-24 10:29:37 +0530

    Updated button component to support links styled as buttons.

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,15 +1,20 @@
 import classNames from 'classnames';
+import { NavLink } from '../';
 
 export default function Button({ children, className, ...otherProps }) {
-  return (
-    <button
-      className={classNames(
-        'p-1 mx-1 mb-1 border-2 border-black dark:border-white dark:bg-black rounded shadow-btn relative',
-        className
-      )}
-      {...otherProps}
-    >
-      {children}
-    </button>
-  );
+  const baseClass =
+    'p-1 mx-1 mb-1 border-2 border-black dark:border-white bg-white dark:bg-black dark:text-white rounded shadow-btn relative';
+  if (otherProps.href) {
+    return (
+      <NavLink className={classNames(baseClass, className)} {...otherProps}>
+        {children}
+      </NavLink>
+    );
+  } else {
+    return (
+      <button className={classNames(baseClass, className)} {...otherProps}>
+        {children}
+      </button>
+    );
+  }
 }
